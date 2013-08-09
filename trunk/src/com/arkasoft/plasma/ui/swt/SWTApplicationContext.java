e9eac263ac314f75894c66545b4bc41f31e6fd17
georgopoulos.georgios@gmail.com <georgopoulos.georgios@gmail.com@6f97281e-d518-c372-eee3-0b5b61c45b3a>
2013-08-09 16:50:08 +0000

    Initial commit (SWT)

---
+++ trunk/src/com/arkasoft/plasma/ui/swt/SWTApplicationContext.java
@@ -0,0 +1,91 @@
+package com.arkasoft.plasma.ui.swt;
+
+import org.eclipse.swt.widgets.Display;
+
+import plasma.application.Application;
+import plasma.stage.StageStyle;
+
+import com.arkasoft.plasma.ApplicationContext;
+import com.arkasoft.plasma.ui.NodePeer;
+import com.arkasoft.plasma.ui.ScenePeer;
+import com.arkasoft.plasma.ui.StagePeer;
+
+public class SWTApplicationContext extends ApplicationContext {
+
+   /**
+    * Utility method to make it easier to define {@code main} entry-points into
+    * applications.
+    * 
+    * @param applicationClass
+    *           the application class that is constructed and executed by the
+    *           launcher.
+    * @param applicationArgs
+    *           the command line arguments passed to the application.
+    */
+   public static void launch(final Class<? extends Application> applicationClass, final String... applicationArgs) {
+      assert ApplicationContext.instance == null : "ApplicationContext already initialized";
+      SWTApplicationContext appContext = new SWTApplicationContext();
+      ApplicationContext.instance = appContext;
+      appContext.launchApplication(applicationClass, applicationArgs);
+   }
+
+   private SWTApplicationContext() {
+      // nothing
+   }
+
+   @Override
+   protected final void launchApplication(final Class<? extends Application> applicationClass, final String... applicationArgs) {
+      super.launchApplication(applicationClass, applicationArgs);
+   }
+
+   private SWTWindow primaryStagePeer = null;
+
+   @Override
+   protected void callApplicationStart() {
+      super.callApplicationStart();
+      if (primaryStagePeer != null && primaryStagePeer.isVisible()) {
+         Display display = Display.getDefault();
+         while (!primaryStagePeer.shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+               display.sleep();
+            }
+         }
+      }
+   }
+
+   @Override
+   protected void runAsync(final Runnable runnable) {
+      Display display = Display.getDefault();
+      display.asyncExec(runnable);
+   }
+
+   @Override
+   protected void syncExec(final Runnable runnable) {
+      Display display = Display.getDefault();
+      display.syncExec(runnable);
+   }
+
+   @Override
+   public StagePeer createStagePeer(StageStyle style, boolean primary) {
+      SWTWindow stagePeer = new SWTWindow(style, primary);
+      if (primary) {
+         primaryStagePeer = stagePeer;
+      }
+      return stagePeer;
+   }
+   
+   @Override
+   public ScenePeer createScenePeer() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public NodePeer createGroupPeer() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+
+
+}
---
+++ trunk/src/com/arkasoft/plasma/ui/swt/SWTView.java
@@ -0,0 +1,101 @@
+package com.arkasoft.plasma.ui.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.opengl.GLCanvas;
+import org.eclipse.swt.opengl.GLData;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+import com.arkasoft.plasma.ui.base.View;
+
+public class SWTView extends View {
+
+   static Shell hiddenShell;
+
+   private final Listener canvasListener = new Listener() {
+      @Override
+      public void handleEvent(Event event) {
+         handleCanvasEvent(event);
+      }
+   };
+
+   Canvas canvas;
+
+   public SWTView() {
+      super();
+
+      if (hiddenShell == null) {
+         hiddenShell = new Shell(Display.getDefault(), SWT.SHELL_TRIM);
+         Display.getDefault().disposeExec(new Runnable() {
+            public void run() {
+               hiddenShell.dispose();
+               hiddenShell = null;
+            }
+         });
+      }
+
+      int bits = SWT.NO_BACKGROUND | SWT.NO_REDRAW_RESIZE | SWT.LEFT_TO_RIGHT; // SWT.NO_FOCUS;
+
+      if (SWT.getPlatform().equals("cocoa")) {
+         GLData data = new GLData();
+         data.doubleBuffer = true;
+         canvas = new GLCanvas(hiddenShell, bits, data);
+      } else {
+         canvas = new Canvas(hiddenShell, bits);
+      }
+
+      int[] events = new int[] {
+            SWT.KeyDown,
+            SWT.KeyUp,
+            SWT.MouseDown,
+            SWT.MouseMove,
+            SWT.MouseEnter,
+            SWT.MouseExit,
+            SWT.MouseHorizontalWheel,
+            SWT.MouseVerticalWheel,
+            SWT.MenuDetect,
+            SWT.Paint,
+            SWT.Resize
+      };
+
+      for (int i = 0; i < events.length; ++i) {
+         canvas.addListener(events[i], canvasListener);
+      }
+
+      // TODO - refactor to drop target creation in a better place
+      // dropTarget = SWTClipboard.createDropTarget(canvas);
+
+      canvas.setData(this);
+   }
+
+   private void handleCanvasEvent(Event event) {
+      switch (event.type) {
+      case SWT.KeyDown:
+         break;
+      case SWT.KeyUp:
+         break;
+      case SWT.MouseDown:
+         break;
+      case SWT.MouseMove:
+         break;
+      case SWT.MouseEnter:
+         break;
+      case SWT.MouseExit:
+         break;
+      case SWT.MouseHorizontalWheel:
+         break;
+      case SWT.MouseVerticalWheel:
+         break;
+      case SWT.MenuDetect:
+         break;
+      case SWT.Paint:
+         break;
+      case SWT.Resize:
+         break;
+      }
+   }
+
+}
