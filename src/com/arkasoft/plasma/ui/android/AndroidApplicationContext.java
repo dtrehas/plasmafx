3290c00e29e716207f8c21b5adbb7b782e8107cf
georgopoulos.georgios@gmail.com <georgopoulos.georgios@gmail.com@6f97281e-d518-c372-eee3-0b5b61c45b3a>
2013-08-09 17:34:38 +0000

    Added AndroidApplicationContext

---
+++ src/com/arkasoft/plasma/ui/android/AndroidApplicationContext.java
@@ -0,0 +1,16 @@
+package com.arkasoft.plasma.ui.android;
+
+import com.arkasoft.plasma.ApplicationContext;
+
+public class AndroidApplicationContext extends ApplicationContext {
+   
+
+   @Override
+   public int getRefreshRate() {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+   
+   
+
+}
