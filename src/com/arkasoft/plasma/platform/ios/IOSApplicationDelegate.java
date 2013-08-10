7811d86f08316355d56d29645d6e31a2fbd552d4
georgopoulos.georgios@gmail.com <georgopoulos.georgios@gmail.com@6f97281e-d518-c372-eee3-0b5b61c45b3a>
2013-08-10 10:23:36 +0000

    Implementation of runSync/runAsync in IOSApplicationDelegate

---
+++ src/com/arkasoft/plasma/platform/ios/IOSApplicationDelegate.java
@@ -0,0 +1,7 @@
+package com.arkasoft.plasma.platform.ios;
+
+import org.robovm.cocoatouch.uikit.UIApplicationDelegate;
+
+public class IOSApplicationDelegate extends UIApplicationDelegate.Adapter {
+
+}
---
+++ src/com/arkasoft/plasma/platform/ios/IOSWindow.java
@@ -0,0 +1,5 @@
+package com.arkasoft.plasma.platform.ios;
+
+public class IOSWindow {
+
+}
