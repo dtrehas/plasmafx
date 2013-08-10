a3186dbcdcd672bfaf84ee6026dc5b8e1ce68c54
georgopoulos.georgios@gmail.com <georgopoulos.georgios@gmail.com@6f97281e-d518-c372-eee3-0b5b61c45b3a>
2013-08-10 10:24:18 +0000

    Implementation of runSync/runAsync in IOSApplicationContext

--- trunk/src/com/arkasoft/plasma/platform/ios/IOSApplicationContext.java
+++ trunk/src/com/arkasoft/plasma/platform/ios/IOSApplicationContext.java
@@ -1,5 +1,10 @@
 package com.arkasoft.plasma.platform.ios;
 
+import org.robovm.cocoatouch.uikit.UIApplication;
+import org.robovm.objc.ObjCRuntime;
+import org.robovm.objc.Selector;
+import org.robovm.rt.VM;
+
 import plasma.application.Application;
 import plasma.stage.StageStyle;
 
@@ -10,71 +15,74 @@ import com.arkasoft.plasma.ui.StagePeer;
 
 public class IOSApplicationContext extends ApplicationContext {
 
-	/**
-	 * Utility method to make it easier to define {@code main} entry-points into
-	 * applications.
-	 * 
-	 * @param applicationClass
-	 *            the application class that is constructed and executed by the
-	 *            launcher.
-	 * @param applicationArgs
-	 *            the command line arguments passed to the application.
-	 */
-	public static void launch(final Class<? extends Application> applicationClass, final String... applicationArgs) {
-		assert ApplicationContext.instance == null : "ApplicationContext already initialized";
-		IOSApplicationContext appContext = new IOSApplicationContext();
-		ApplicationContext.instance = appContext;
-		appContext.launchApplication(applicationClass, applicationArgs);
-	}
-	
-	private IOSApplicationContext() {
-		// nothing
-	}
-	
-   @Override
-   protected void runEventLoop() {
-      // TODO Auto-generated method stub
-      
+   /**
+    * Utility method to make it easier to define {@code main} entry-points into
+    * applications.
+    * 
+    * @param applicationClass
+    *           the application class that is constructed and executed by the
+    *           launcher.
+    * @param applicationArgs
+    *           the command line arguments passed to the application.
+    */
+   public static void launch(IOSApplicationDelegate appDelegate, final Class<? extends Application> applicationClass, final String... applicationArgs) {
+      assert ApplicationContext.instance == null : "ApplicationContext already initialized";
+      IOSApplicationContext appContext = new IOSApplicationContext(appDelegate);
+      ApplicationContext.instance = appContext;
+      appContext.launchApplication(applicationClass, applicationArgs);
    }
 
-	@Override
-	protected void runAsync(Runnable runnable) {
-		// TODO Auto-generated method stub
+   private final IOSApplicationDelegate appDelegate;
 
-	}
+   private Thread dispatchThread;
 
-	@Override
-	protected void syncExec(Runnable launchable) {
-		// TODO Auto-generated method stub
+   private IOSApplicationContext(IOSApplicationDelegate appDelegate) {
+      this.appDelegate = appDelegate;
+   }
 
-	}
+   @Override
+   protected void runEventLoop() {
 
-	@Override
-	public StagePeer createStagePeer(StageStyle style, boolean primary) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   }
 
-	@Override
-	public ScenePeer createScenePeer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   protected void runAsync(Runnable runnable) {
+      IOSRunnable r = new IOSRunnable(runnable);
+      r.performOnMainThread(false);
+   }
 
-	@Override
-	public NodePeer createGroupPeer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+   @Override
+   protected void syncExec(Runnable runnable) {
+      IOSRunnable r = new IOSRunnable(runnable);
+      r.performOnMainThread(true);
+   }
+
+   @Override
+   public StagePeer createStagePeer(StageStyle style, boolean primary) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public ScenePeer createScenePeer() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public NodePeer createGroupPeer() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
    @Override
    public boolean hasTouch() {
-       return true;
+      return true;
    }
 
    @Override
    public boolean hasMultiTouch() {
-       return true;
+      return true;
    }
 
 }
